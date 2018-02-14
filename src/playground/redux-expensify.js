dca590284da2f323cbc70c5e4ee9935696ff2bb3
Jordy Guntur <jguntur@u.rochester.edu>
2018-02-14 14:50:33 -0500

    action generators cont. spread ops

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,6 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import uuid from 'uuid';
 
+/*
+	ACTION GENERATORS
+*/
+/* ######################################################### */
 // ADD_EXPENSE
 const addExpense = (
 	{
@@ -16,33 +20,72 @@ const addExpense = (
 		description,
 		note,
 		amount,
-		createdAt
+		createdAt	
 	}
 });
 
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+	type: 'REMOVE_EXPENSE',
+	id
+});
+
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+	type: 'EDIT_EXPENSE',
+	id,
+	updates
+});
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+	type: 'SET_TEXT_FILTER',
+	text
+});
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
 // SET_END_DATE
 
-// Expenses Reducer
+/* ######################################################### */
 
+
+
+
+/*
+	REDUCERS
+*/
+/* ######################################################### */
+// Expenses Reducer
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
 	switch (action.type) {
 		case 'ADD_EXPENSE':
-			return state.concat(action.expense);
+			return [
+			...state,
+			action.expense
+			];
+		case 'REMOVE_EXPENSE':
+			return state.filter(({ id }) => id !== action.id);
+		case 'EDIT_EXPENSE':
+			return state.map((expense) => {
+				if(expense.id === action.id) {
+					return {
+						...state,
+						...action.updates
+					};
+				} else {
+					return expense;
+				}
+			});
 		default:
 			return state;
 	}
 }
 
 // Filters Reducer
-
 const filtersReducerDefaultState = {
 	text: '',
 	sortBy: 'date',
@@ -52,28 +95,53 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
 	switch(action.type) {
+		case 'SET_TEXT_FILTER':
+			return {
+				...state,
+				text: action.text
+			}
 		default:
 			return state;
 	}
 };
+/* ######################################################### */
 
 
-// Store Creation
 
+
+/*
+	STORE
+*/
+/* ######################################################### */
 const store = createStore(
 	combineReducers({
 		expenses: expensesReducer,
 		filter: filtersReducer
 	})
 );
+/* ######################################################### */
 
+
+
+// Log updates to store
 store.subscribe(() => {
 	console.log(store.getState());
 });
 
-store.dispatch(addExpense({ description: 'rent', amount: 100 }));
+
+// Dispatching Actions
+// Adding expenses
+const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'coffee', amount: 300 }));
+// Removing expenses
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// Editing expenses and text filter
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 
+// Example State
 const demoState = {
 	expenses: [{
 		id: 'testID',
@@ -89,3 +157,14 @@ const demoState = {
 		endDate: undefined
 	}
 };
+
+// ES6 Spread Operator for Objects
+const user = {
+	name: "Jordy",
+	age: 23
+};
+
+console.log({
+	...user,
+	age: 55
+});
